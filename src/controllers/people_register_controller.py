8931dfdc018a4ebcbf4ddcdbb81d8d12c8a8e002
itallo.minatti <itallominatti@gmail.com>
2024-12-04 09:53:44 -0300

    [ feat ] develop -> implementando controller para registro de usuário

---
+++ src/controllers/people_register_controller.py
@@ -0,0 +1,34 @@
+from typing import Dict
+
+class PeopleRegisterController:
+    def register(self, new_person_informations: Dict) -> Dict:
+        try:
+            self.__validate_fields(new_person_informations)
+            response = self.__format_response(new_person_informations)
+            return {
+                "success": True,
+                "message": response
+            }
+        except Exception as exception:
+            return {
+                "success": False,
+                "error": str(exception)
+            }
+
+    def __validate_fields(self, new_person_informations: Dict) -> None:
+        if not isinstance(new_person_informations['name'], str):
+            raise Exception('Campo Nome Incorreto!')
+        
+        try: int(new_person_informations['age'])
+        except: raise Exception('Campo Idade Incorreto!')
+
+        try: int(new_person_informations['height']) \
+            or float(new_person_informations['height'])
+        except: raise Exception('Campo Altura Incorreto!')
+
+    def __format_response(self, new_person_informations: Dict) -> Dict:
+        return {
+            "count": 1,
+            "type": "person",
+            "attributes": new_person_informations
+        }
